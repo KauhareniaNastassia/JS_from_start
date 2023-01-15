cad4410b91990beda15315c1acb7c6125ca2ca54
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-01-15 17:39:03 +0300

    add 2.Fundamentals_JS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import style from './App.module.scss'
-import Header from "./Header/Header";
-import Main from "./Main/Main";
-import About from "./Skills/About";
-import Projects from "./Projects/Projects";
-import Contacts from "./Contacts/Contacts";
-import Photo from "./Photo/Photo";
-import Skills from "./Skills/Skills/Skills";
 
 function App() {
     return (
         <div className={style.appWrapper}>
-            <Header/>
-            <Photo/>
-            <div className={style.appWrapperContent}>
-                <Main/>
-                <About/>
-                <Skills/>
-                <Projects/>
-                <Contacts/>
-            </div>
+
         </div>
     );
 }
