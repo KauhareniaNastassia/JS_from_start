4bb9f0afd697d9e7d041fb5e1c9f65d83ba714e9
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-01-16 14:05:40 +0300

    add 3.Objects

---
+++ src/LearnJS/4.Objects(Fundamentals).js
@@ -0,0 +1,48 @@
+//========== OBJECTS FUNDAMENTALS ==========
+
+/*let user = {}
+user.name = 'John'
+user.surname = 'Smith'
+user.name = 'Pete'
+delete user.name*/
+
+
+
+/*
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}*/
+
+
+/*
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+let sum = 0
+for (let key in salaries) {
+    sum += salaries[key]
+}*/
+
+
+
+
+/*
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(menu) {
+    for (let key in menu) {
+        if (typeof menu[key] == 'number') {
+            menu[key] *= 2
+        }
+    }
+}*/
