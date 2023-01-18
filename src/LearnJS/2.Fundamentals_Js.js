0e2a282b66f9a5ec6eecccc06ae71194641dfff0
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-01-18 15:23:50 +0300

    practice Numbers

---
+++ src/LearnJS/2.Fundamentals_Js.js
@@ -0,0 +1,95 @@
+
+//==========SWITCH==========
+
+
+/*if(browser == 'Edge') {
+    alert("You've got the Edge!");
+} else if (browser == 'Chrome'
+    || browser == 'Firefox'
+    || browser == 'Safari'
+    || browser == 'Opera') {
+    alert( 'Okay we support these browsers too' );
+} else {
+    alert( 'We hope that this page looks ok!' );
+}*/
+
+
+/*
+const number = +prompt('Введите число между 0 и 3', '');
+
+
+switch(number) {
+    case 0:
+        alert('Вы ввели число 0')
+        break;
+    case 1:
+        alert('Вы ввели число 1')
+        break;
+    case 2 || 3:
+        alert('Вы ввели число 2, а может и 3')
+        break;
+}*/
+
+
+//==========FUNCTIONS==========
+
+// eslint-disable-next-line no-restricted-globals
+/*let conf = confirm('Родители разрешили?')
+
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        return conf;
+    }
+}
+
+function checkAge1(age) {
+    return (age > 18) ? true : conf
+}
+
+function chackAge2(age) {
+    return (age > 18) || conf
+}*/
+
+
+
+/*function min(a,b) {
+    return (a < b) ? a : b
+}*/
+
+
+
+/*let x = prompt('Введите x')
+let n = prompt('Введите n')
+
+function pow(x,n) {
+    if(n > 1) {
+        return alert(Math.pow(x,n))
+    } else {
+        return alert('Попробуй еще раз)')
+    }
+}
+
+pow(x,n)*/
+
+
+
+
+
+//==========ARROW FUNCTION ==========
+
+/*function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+
+ask(
+    "Вы согласны?",
+    () => { alert("Вы согласились."); },
+    () => { alert("Вы отменили выполнение."); }
+);*/
+
+
+
+
